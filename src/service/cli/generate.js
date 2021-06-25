bff2fb02506d731625f2889aa45fce002c3b4552
evgenii.grushin <evgenii.grushin@fortify.pro>
2021-06-24 23:03:01 -0700

    add

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -9,31 +9,48 @@ const {
 } = require(`../utils`);
 
 const {
-  CATEGORIES,
-  SENTENCES,
-  TITLES,
+  // CATEGORIES,
+  // SENTENCES,
+  // TITLES,
+  FILE_SENTENCES_PATH,
+  FILE_TITLES_PATH,
+  FILE_CATEGORIES_PATH,
   SumRestrict,
   DEFAULT_COUNT,
   FILE_NAME,
   ExitCode
 } = require(`../constants`);
 
-const generateOffers = (count) => (
+const readContent = async (filePath) => {
+  try {
+    const content = await fs.readFile(filePath, `utf8`);
+    return content.trim().split(`\n`);
+  } catch (err) {
+    console.error(chalk.red(err));
+    return [];
+  }
+};
+
+const generateOffers = (count, titles, categories, sentences) => (
   Array(count).fill({}).map(() => ({
-    title: TITLES[getRandomInt(0, TITLES.length - 1)],
+    title: titles[getRandomInt(0, titles.length - 1)],
     createdDate: new Date(getRandomInt(SumRestrict.MIN, SumRestrict.MAX)).toLocaleDateString(`en-US`),
-    announce: shuffle(SENTENCES).slice(1, 5).join(` `),
-    fullText: shuffle(SENTENCES).slice(1, 5).join(` `),
-    category: [CATEGORIES[getRandomInt(0, CATEGORIES.length - 1)]],
+    announce: shuffle(sentences).slice(1, 5).join(` `),
+    fullText: shuffle(sentences).slice(1, 5).join(` `),
+    category: [categories[getRandomInt(0, categories.length - 1)]],
   }))
 );
 
 module.exports = {
   name: `--generate`,
   async run(args) {
+    const sentences = await readContent(FILE_SENTENCES_PATH);
+    const titles = await readContent(FILE_TITLES_PATH);
+    const categories = await readContent(FILE_CATEGORIES_PATH);
+
     const [count] = args;
     const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
-    const content = JSON.stringify(generateOffers(countOffer));
+    const content = JSON.stringify(generateOffers(countOffer, titles, categories, sentences));
 
     try {
       await fs.writeFile(FILE_NAME, content);
