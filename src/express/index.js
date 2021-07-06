a90d7add5a25e889ada911166ed66c99222baac4
evgenii.grushin <evgenii.grushin@fortify.pro>
2021-07-06 10:04:13 -0700

    started task

--- src/express/index.js
+++ src/express/index.js
@@ -1,17 +1,22 @@
 'use strict';
 
 const express = require(`express`);
-
 const articlesRoutes = require(`./routes/articles-routes`);
 const myRoutes = require(`./routes/my-routes`);
 const mainRoutes = require(`./routes/main-routes`);
 
-const DEFAULT_PORT = 8080;
+const DEFAULT_PORT = 8084;
 
 const app = express();
+const path = require(`path`);
+const PUBLIC_DIR = `public`;
 
 app.use(`/articles`, articlesRoutes);
 app.use(`/my`, myRoutes);
 app.use(`/`, mainRoutes);
 
 app.listen(DEFAULT_PORT);
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+
+app.set(`views`, path.resolve(__dirname, `templates`));
+app.set(`view engine`, `pug`);
--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -5,7 +5,7 @@ const mainRoutes = new Router();
 
 mainRoutes.get(`/`, (req, res) => res.send(`/`));
 mainRoutes.get(`/register`, (req, res) => res.send(`/register`));
-mainRoutes.get(`/login`, (req, res) => res.send(`/login`));
+mainRoutes.get(`/login`, (req, res) => res.render(`/login`));
 mainRoutes.get(`/search`, (req, res) => res.send(`/search`));
 mainRoutes.get(`/categiries`, (req, res) => res.send(`/categiries`));
 
