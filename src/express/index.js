53aeb867bdd8d903813308a35e9cb2af0cc29a95
evgenii.grushin <evgenii.grushin@fortify.pro>
2021-07-13 23:55:38 -0700

    add

--- src/express/index.js
+++ src/express/index.js
@@ -5,7 +5,7 @@ const articlesRoutes = require(`./routes/articles-routes`);
 const myRoutes = require(`./routes/my-routes`);
 const mainRoutes = require(`./routes/main-routes`);
 
-const DEFAULT_PORT = 6065;
+const DEFAULT_PORT = 8080;
 
 const app = express();
 const path = require(`path`);
@@ -15,8 +15,19 @@ app.use(`/articles`, articlesRoutes);
 app.use(`/my`, myRoutes);
 app.use(`/`, mainRoutes);
 
+
 app.listen(DEFAULT_PORT);
 app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
 
 app.set(`view engine`, `pug`);
 app.set(`views`, path.resolve(__dirname, `templates`));
+
+app.use(function (req, res) {
+  res.status(400);
+  res.render(`errors/404`);
+});
+
+app.use(function (req, res) {
+  res.status(500);
+  res.render(`errors/500`);
+});
