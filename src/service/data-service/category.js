5fd08970a5c02ac9a991b0ceae95eb99ca3af7e2
evgenii.grushin <evgenii.grushin@fortify.pro>
2021-12-11 00:52:32 -0800

    add

--- src/service/data-service/category.js
+++ src/service/data-service/category.js
@@ -1,10 +1,14 @@
+/* eslint-disable eqeqeq */
+/* eslint-disable camelcase */
 'use strict';
 
+const Aliase = require(`../models/aliase`);
 
 class CategoryService {
   constructor(sequelize) {
     this._Article = sequelize.models.Article;
     this._Category = sequelize.models.Category;
+    // this._ArticleCategory = sequelize.models.ArticleCategory;
   }
 
   async findAll() {
@@ -12,6 +16,27 @@ class CategoryService {
   }
   async findOne(categoryId) {
     return this._Category.findByPk(categoryId);
+  }
+  async findPage(categoryId) {
+    const include = [
+      Aliase.CATEGORIES
+    ];
+
+    const articles = await this._Article.findAll({
+      include
+    });
+
+    const articlesByCategory = [];
+
+    articles.map((article) => {
+      article.categories.map((category) => {
+        if (category.id == categoryId) {
+          articlesByCategory.push(article);
+        }
+      });
+    });
+
+    return articlesByCategory;
   }
 }
 
